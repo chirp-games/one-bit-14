2b0477180f20ccf7795eae07e287bd5fff181859
Tyrannicodin <tyrannicodin@gmail.com>
2026-09-14 20:11:48 +0100

    Basic fp controls

---
+++ odins_tests/character_body_3d.gd
@@ -0,0 +1,45 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const LOOK_VELOCITY_Y = 0.01
+
+
+@onready var camera = %Camera
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var mouse_event := event as InputEventMouseMotion
+		rotate_y(
+			camera.rotation_degrees.y + mouse_event.relative.x * -LOOK_VELOCITY_Y
+		)
+		camera.rotate_x(
+			mouse_event.relative.y * -LOOK_VELOCITY_Y
+		)
+		camera.rotation.x = clampf(camera.rotation.x, -PI, PI)
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
