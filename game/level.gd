e9912288e78580a10d1ff2facb356bc2014ea08a
Zunda <mailme@jacobhaussman.online>
2026-09-15 13:35:42 -0400

    Moving platform

--- game/level.gd
+++ game/level.gd
@@ -3,10 +3,8 @@ extends Node3D
 
 func _on_button_1_pushed() -> void:
 	print("pushed")
+	%PathPlatform.movement_mode = PathFollowPlatform.MovementMode.Forward
 
 func _on_button_1_released() -> void:
 	print("released")
-
-func _physics_process(delta: float) -> void:
-	if %Button1.is_pushed:
-		%Platform.apply_central_force(Vector3(0, 1000, 0))
+	%PathPlatform.movement_mode = PathFollowPlatform.MovementMode.Back
---
+++ game/objects/path_follow_platform.gd
@@ -0,0 +1,20 @@
+class_name PathFollowPlatform
+
+extends PathFollow3D
+
+@export var speed = .05
+
+enum MovementMode {
+	Stopped,
+	Forward,
+	Back,
+}
+
+@export var movement_mode: MovementMode = MovementMode.Stopped
+
+func _physics_process(delta: float) -> void:
+	if movement_mode == MovementMode.Forward:
+		progress_ratio = clamp(progress_ratio + speed * delta, 0, 1)
+	if movement_mode == MovementMode.Back:
+		progress_ratio = clamp(progress_ratio - speed * delta, 0, 1)
+	%Platform.global_position = global_position
