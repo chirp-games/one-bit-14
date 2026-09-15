4b1574eab1e09edab209ea2a3d0897c2f517576e
Zunda <mailme@jacobhaussman.online>
2026-09-15 14:36:56 -0400

    Fix things being flung by setting max force black holes can give

--- jakes_test/center_of_gravity.gd
+++ jakes_test/center_of_gravity.gd
@@ -16,13 +16,10 @@ func _physics_process(_delta: float) -> void:
 
 	for body in get_tree().get_nodes_in_group("gravity_body"):
 		var dist = body.global_position.distance_to(global_position)
+		var force = gravity * (1. / float(pow(dist, 2)))
+		if force > gravity:
+			force = gravity
 
-		var apply_gravity = gravity * (1. / float(pow(dist, 2.))) * body.global_position.direction_to(global_position)
-		
+		var apply_gravity = force * body.global_position.direction_to(global_position)
 		if body is RigidBody3D:
 			body.apply_force(apply_gravity)
-		
-		if body.is_in_group("player"):
-			if body.global_position.distance_to(global_position) < .1:
-				print("You died")
-				get_tree().quit()
