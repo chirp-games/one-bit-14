2c78d60bbf6775ce1e32306d22ef44ea8739877c
Zunda <mailme@jacobhaussman.online>
2026-09-15 10:18:58 -0400

    make black holes placeable

--- game/game.gd
+++ game/game.gd
@@ -5,3 +5,6 @@ extends Control
 func _ready() -> void:
 	%DitherViewport.texture = %Player.dither_viewport.get_texture()
 	%OutlineViewport.texture = %Player.outline_viewport.get_texture()
+
+func _on_player_create_black_hole(pos: Vector3) -> void:
+	%BlackHole.global_position = pos
--- game/objects/rigid_player.gd
+++ game/objects/rigid_player.gd
@@ -1,5 +1,7 @@
 extends RigidBody3D
 
+signal create_black_hole(position: Vector3)
+
 const WALK_FORCE = 30
 const AIR_WALK_FORCE = 10
 const JUMP_IMPULSE = 5
@@ -7,8 +9,8 @@ const LOOK_VELOCITY_Y = 0.01
 const CAYOTE_TIME = .1
 const MAX_GROUND_VELOCTIY = 7
 
-const MIN_KICK_POWER = 1
-const MAX_KICK_POWER = 5
+const MIN_KICK_POWER = .5
+const MAX_KICK_POWER = 2
 const KICK_ANGLE_FROM_GROUND = PI / 8
 
 var cayote_timer = 0
@@ -59,6 +61,9 @@ func release_kick():
 		var rotated_impulse = impulse * direction
 		obj.apply_impulse(rotated_impulse)
 
+func position_black_hole_preview():
+	%BlackHolePreview.global_position = %CreateBlackHoleRay.global_position + %CreateBlackHoleRay.global_position.direction_to(%CreateBlackHoleRay.global_transform * Vector3.FORWARD) * -%CreateBlackHoleRay.target_position.z
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -73,6 +78,14 @@ func _input(event: InputEvent) -> void:
 			mouse_event.relative.y * -LOOK_VELOCITY_Y
 		)
 		camera.rotation.x = clampf(camera.rotation.x, -PI/2, PI/2)
+
+	if event.is_action("scroll_up"):
+		%CreateBlackHoleRay.target_position.z -= .1
+	if event.is_action("scroll_down"):
+		%CreateBlackHoleRay.target_position.z += .1
+		
+	if event.is_action_pressed("click"):
+		create_black_hole.emit(%BlackHolePreview.global_position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -121,7 +134,8 @@ func _process(delta: float) -> void:
 			release_kick()
 			current_kick_power = 0
 			targeting_kick_obj = null
-	
+
+	position_black_hole_preview()
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	# https://forum.godotengine.org/t/how-to-check-if-rigid-body-is-on-floor/65679/3
