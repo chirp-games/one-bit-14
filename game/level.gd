866725092b1f6df575976dfea26ff363cf40a6f5
Zunda <mailme@jacobhaussman.online>
2026-09-15 13:57:24 -0400

    Add door

--- game/level.gd
+++ game/level.gd
@@ -4,7 +4,9 @@ extends Node3D
 func _on_button_1_pushed() -> void:
 	print("pushed")
 	%PathPlatform.movement_mode = PathFollowPlatform.MovementMode.Forward
+	%Door.open()
 
 func _on_button_1_released() -> void:
 	print("released")
 	%PathPlatform.movement_mode = PathFollowPlatform.MovementMode.Back
+	%Door.close()
