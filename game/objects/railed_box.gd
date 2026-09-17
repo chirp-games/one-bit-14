4679655be3b1659e9988e97e8875f1ce6ba0eaea
Zunda <mailme@jacobhaussman.online>
2026-09-17 17:25:51 -0400

    add start offset for boxes

--- game/objects/railed_box.gd
+++ game/objects/railed_box.gd
@@ -4,6 +4,9 @@ class_name RailedBox
 
 extends Node3D
 
+# Percent offset to start box at
+@export var start_offset: float
+
 var path: Path3D
 
 func _ready() -> void:
@@ -16,3 +19,5 @@ func _ready() -> void:
 
 	var dist = path.curve.get_point_position(0).distance_to(path.curve.get_point_position(1))
 	%SliderJoint3D.set_param(SliderJoint3D.PARAM_LINEAR_LIMIT_UPPER, dist - .25)
+
+	%RailedBox.global_position = path.global_position + start_offset * path.curve.get_point_position(1)
