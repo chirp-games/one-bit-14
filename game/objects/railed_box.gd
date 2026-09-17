d6532212046ad42fc7e881f0f531fcf42b44db35
Zunda <mailme@jacobhaussman.online>
2026-09-17 16:02:11 -0400

    collision adjustments

--- game/objects/railed_box.gd
+++ game/objects/railed_box.gd
@@ -8,11 +8,11 @@ var path: Path3D
 
 func _ready() -> void:
 	path = get_parent()
-	
-	if path != null:
-		%PathMesh3D.path_3d = path
 
-	%PathMesh3D.global_rotation = Vector3(0, 0, 0)
+	if path != null:
+		%Rail.path_3d = path
+
+	%Rail.global_rotation = Vector3(0, 0, 0)
 
 	var dist = path.curve.get_point_position(0).distance_to(path.curve.get_point_position(1))
 	%SliderJoint3D.set_param(SliderJoint3D.PARAM_LINEAR_LIMIT_UPPER, dist - 1)
