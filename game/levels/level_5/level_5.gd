a9744627b8a8636aae325c5b5d8018c9093b16b3
Tyrannicodin <tyrannicodin@gmail.com>
2026-09-18 00:08:35 +0100

    Level 5 completion

---
+++ game/levels/level_5/level_5.gd
@@ -0,0 +1,35 @@
+extends "res://game/levels/nologic_level.gd"
+
+
+@export var hold_time := 3.
+
+var progressing := false
+var time := 0.
+var last_second := -1
+
+var first = true
+
+func succeeding() -> void:
+	progressing = true
+
+func failing() -> void:
+	if first:
+		first = false
+		return
+	progressing = false
+	time = 0
+	last_second = -1
+	%Failure.play()
+
+func _process(delta: float) -> void:
+	if progressing:
+		time += delta
+		
+		if last_second != floor(time):
+			last_second = floor(time)
+			%Bleeper.pitch_scale = 1 + last_second * 0.25
+			%Bleeper.play()
+			
+		if time > hold_time:
+			await %Bleeper.finished
+			level_complete.emit()
