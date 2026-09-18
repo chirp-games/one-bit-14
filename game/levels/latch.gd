374d198a739df72c7cdf0488552804fabda20956
Zunda <mailme@jacobhaussman.online>
2026-09-18 18:27:55 -0400

    wires

--- game/levels/latch.gd
+++ game/levels/latch.gd
@@ -5,12 +5,21 @@ func _on_button_pushed() -> void:
 	print("pushed")
 	%Door1.close()
 	%Door2.open()
-
+	
+	for w in $Wires1.get_children():
+		w.power_on()
+	for w in $Wires2.get_children():
+		w.power_on()
 
 func _on_button_released() -> void:
 	print("released")
 	%Door1.open()
 	%Door2.close()
+
+	for w in $Wires1.get_children():
+		w.power_off()
+	for w in $Wires2.get_children():
+		w.power_off()
 
 
 func _on_completion_area_level_complete() -> void:
---
+++ game/objects/wire_connections/wired_tile.gd
@@ -0,0 +1,51 @@
+@tool
+
+extends Node3D
+
+@export var powered = false:
+	set(v):
+		powered = v
+		if powered:
+			power_on()
+		else:
+			power_off()
+	get():
+		return powered
+
+@export var type: Type
+
+enum Type {
+	Straight,
+	Bend,
+}
+
+var powered_material = load("res://assets/materials/wire_on.tres")
+var off_material = load("res://assets/materials/wire_off.tres")
+
+@onready var all_nodes = $Wires.get_children()
+
+func hide_all():
+	for node in all_nodes:
+		node.hide()
+
+func power_on():
+	for node in all_nodes:
+		node.set_surface_override_material(0, powered_material)
+
+func power_off():
+	for node in all_nodes:
+		node.set_surface_override_material(0, off_material)
+
+func _ready() -> void:
+	if powered:
+		power_on()
+	else:
+		power_off()
+
+	if type == Type.Straight:
+		hide_all()
+		%straight.show()
+	if type == Type.Bend:
+		hide_all()
+		%bend.show()
+		%bend_2.show()
