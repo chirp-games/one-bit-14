26a8d5864c6f288d1706e1a251118584b95c13ed
Benji42 <cheezyanimations@gmail.com>
2026-09-19 13:48:26 -0400

    lol

--- game/objects/hanging_light.gd
+++ game/objects/hanging_light.gd
@@ -1,16 +1,1 @@
-<<<<<<< HEAD
 extends Node3D
-=======
-@tool
-
-extends Node3D
-
-@export var length: float = 3.0
-
-func _enter_tree() -> void:
-	%String.mesh = %String.mesh.duplicate()
-	
-	%Bulb.position.y = -length
-	%String.mesh.size.y = length
-	%String.position.y = length / 2.
->>>>>>> d61e04ae67e6687eaacc560659c880dd8f293521
