ecf323f488306e1b5b7cdbc000e5575fd346f5cf
Zunda <mailme@jacobhaussman.online>
2026-09-19 19:48:55 -0400

    flak cannon

---
+++ game/levels/flak_cannon.gd
@@ -0,0 +1,26 @@
+extends "res://game/levels/nologic_level.gd"
+
+
+func _on_button_1_pushed() -> void:
+	%Door1.open()
+	%Door3.open()
+	for w in %Wires2.get_children():
+		w.power_on()
+
+
+func _on_button_1_released() -> void:
+	%Door1.close()
+	%Door3.close()
+	for w in %Wires2.get_children():
+		w.power_off()
+
+func _on_button_2_pushed() -> void:
+	%Door2.open()
+	for w in %Wires1.get_children():
+		w.power_on()
+
+
+func _on_button_2_released() -> void:
+	%Door2.close()
+	for w in %Wires1.get_children():
+		w.power_off()
