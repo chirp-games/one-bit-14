4d173aca96de38f10c2f722964300d152d9c9afc
Tyrannicodin <tyrannicodin@gmail.com>
2026-09-19 16:37:40 +0100

    Fix world bottom

--- game/game.gd
+++ game/game.gd
@@ -65,6 +65,6 @@ func _on_player_create_black_hole(pos: Vector3) -> void:
 func _on_player_delete_black_hole() -> void:
 	%BlackHole.global_position = Vector3(0, -10000, 0)
 
-func _physics_process(delta: float) -> void:
-	if %Player.position.y < -5:
+func _physics_process(_delta: float) -> void:
+	if %Player.position.y < (current_level.floor if current_level else -5):
 		reset()
