2b095760eccef7d20abc3f807119e7fae0a81b1e
Zunda <mailme@jacobhaussman.online>
2026-09-19 00:25:33 -0400

    ?

--- game/levels/laser_walls.gd
+++ game/levels/laser_walls.gd
@@ -1,39 +1,5 @@
 extends Node3D
 
-func _ready() -> void:
-	for c in %Wires1.get_children():
-		c.power_on()
-	for c in %Wires2.get_children():
-		c.power_on()
-	 
-func _on_laser_reciever_1_pushed() -> void:
-	for c in %Wall1.get_children():
-		c.turn_on()
-	for c in %Wires1.get_children():
-		c.powered = true
-
-
-func _on_laser_reciever_1_released() -> void:
-	for c in %Wall1.get_children():
-		c.turn_off()
-	for c in %Wires1.get_children():
-		c.powered = false
-
-func _on_laser_reciever_2_pushed() -> void:
-	for c in %Wall2.get_children():
-		c.turn_on()
-	for c in %Wires2.get_children():
-		c.powered = true
-
-
-func _on_laser_reciever_2_released() -> void:
-	for c in %Wall2.get_children():
-		c.turn_off()
-	for c in %Wires2.get_children():
-		c.powered = false
-
-
-
 func _on_button_2_pushed() -> void:
 	%Door.open()
 
