477dc2a0ec7fa207696a64b8381c19f14b9da0cc
Zunda <mailme@jacobhaussman.online>
2026-09-20 11:55:15 -0400

    start redoing door walk

--- game/levels/nologic_level.gd
+++ game/levels/nologic_level.gd
@@ -4,4 +4,11 @@ signal level_complete
 
 func on_complete() -> void:
 	level_complete.emit()
-pass # Replace with function body.
+
+
+func _on_button_1_pushed() -> void:
+	%Door1.open()
+
+
+func _on_button_1_released() -> void:
+	%Door1.close()
