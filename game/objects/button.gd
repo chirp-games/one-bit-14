2a098b49cb141e4bd0148abfcb59075bc9a2d26d
Benji42 <cheezyanimations@gmail.com>
2026-09-20 12:09:40 -0400

    sound effects not working

--- game/objects/button.gd
+++ game/objects/button.gd
@@ -12,11 +12,13 @@ func _physics_process(_delta: float) -> void:
 		released_signal_emitted = false
 		if not pushed_signal_emitted:
 			pushed.emit()
+			$ClickOn.play()
 			pushed_signal_emitted = true
 			is_pushed = true
 	else:
 		pushed_signal_emitted = false
 		if not released_signal_emitted:
 			released.emit()
+			$ClickOff.play()
 			released_signal_emitted = true
 			is_pushed = false
