9af4fdb654e6532d964b2bd19583f64c01c05d0c
Zunda <mailme@jacobhaussman.online>
2026-09-20 11:14:52 -0400

    give laser recievers a glow up

--- game/objects/reciever_top.gd
+++ game/objects/reciever_top.gd
@@ -7,6 +7,9 @@ var is_pushed: bool = false
 var pushed_signal_emitted = false
 var released_signal_emitted = true
 
+var powered_material = load("res://assets/materials/wire_on.tres")
+var off_material = load("res://assets/materials/wire_off.tres")
+
 func laser_hit() -> void:
 	is_pushed = true
 
@@ -15,10 +18,14 @@ func _physics_process(_delta: float) -> void:
 		released_signal_emitted = false
 		if not pushed_signal_emitted:
 			pushed.emit()
+			for node in %change_material.get_children():
+				node.set_surface_override_material(0, powered_material)
 			pushed_signal_emitted = true
 	else:
 		pushed_signal_emitted = false
 		if not released_signal_emitted:
+			for node in %change_material.get_children():
+				node.set_surface_override_material(0, off_material)
 			released.emit()
 			released_signal_emitted = true
 	
