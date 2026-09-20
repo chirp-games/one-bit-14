9ec6cd8f81871c68596964d35ee0891d4d918aad
Zunda <mailme@jacobhaussman.online>
2026-09-19 23:48:53 -0400

    make room cool

--- game/levels/level_1.gd
+++ game/levels/level_1.gd
@@ -3,11 +3,14 @@ extends "res://game/levels/nologic_level.gd"
 
 func _on_button_1_pushed() -> void:
 	%Door1.open()
+	%wire1.power_on()
+	%wire2.power_on()
 
 
 func _on_button_1_released() -> void:
 	%Door1.close()
-
+	%wire1.power_off()
+	%wire2.power_off()
 
 func _on_completion_area_level_complete() -> void:
 	on_complete()
