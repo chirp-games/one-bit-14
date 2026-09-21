64cd8a4fa2122f10c52a2b1dfb483e91eaf31e89
Tyrannicodin <tyrannicodin@gmail.com>
2026-09-21 01:20:13 +0100

    Prevent mouse capture in editor

--- game/game.gd
+++ game/game.gd
@@ -60,8 +60,8 @@ func _ready() -> void:
 	if not Engine.is_editor_hint():
 		loop_music()
 	
-	if not OS.has_feature("web"):
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		if not OS.has_feature("web"):
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 	reset()
 
