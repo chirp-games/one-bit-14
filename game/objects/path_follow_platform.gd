60a7674776ae4618a29812952eea5581c0bd2313
Tyrannicodin <tyrannicodin@gmail.com>
2026-09-21 18:52:24 +0100

    Jump rope

--- game/objects/path_follow_platform.gd
+++ game/objects/path_follow_platform.gd
@@ -12,11 +12,27 @@ enum MovementMode {
 
 @export var movement_mode: MovementMode = MovementMode.Stopped
 
+@export var normalise_speed := false
+
+@onready var parent: Path3D = get_parent()
+
 func _physics_process(delta: float) -> void:
 	if movement_mode == MovementMode.Forward:
-		progress_ratio = clamp(progress_ratio + speed * delta, 0, 1)
+		progress_ratio = clamp(
+			progress_ratio +
+				((speed / parent.curve.get_baked_length()) if normalise_speed else speed) *
+				delta,
+			0,
+			1
+		)
 	if movement_mode == MovementMode.Back:
-		progress_ratio = clamp(progress_ratio - speed * delta, 0, 1)
+		clamp(
+			progress_ratio -
+				((speed / parent.curve.get_baked_length()) if normalise_speed else speed) *
+				delta,
+			0,
+			1
+		)
 	%Platform.global_position = global_position
 
 func stop() -> void:
