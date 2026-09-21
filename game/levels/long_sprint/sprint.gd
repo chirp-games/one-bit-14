2b4ce0f8c9e505195631b9f48c54d8b8cb8b2ecd
Tyrannicodin <tyrannicodin@gmail.com>
2026-09-21 13:50:45 +0100

    Update icon, leeway for sprint and fix l3

--- game/levels/long_sprint/sprint.gd
+++ game/levels/long_sprint/sprint.gd
@@ -10,12 +10,13 @@ var ticks = 0
 
 
 func timer_tick() -> void:
-	if ticks > time_limit:
+	if ticks >= time_limit:
 		return
 		
 	ticks += 1
 	if ticks == time_limit:
 		$Fallout.collision_layer = 0 # Turns off collisions
+		$Fallout.position = Vector3(0, 100, 0)
 		$Fallout.hide()
 		
 	$Audio.pitch_scale = pitch_curve.sample(ticks / time_limit) * max_pitch + min_pitch
