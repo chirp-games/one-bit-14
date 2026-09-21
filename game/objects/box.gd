26935b9e9f1a5b8bee91b1e5ac268cd68dccc262
Tyrannicodin <tyrannicodin@gmail.com>
2026-09-21 18:12:08 +0100

    Button on a box

--- game/objects/box.gd
+++ game/objects/box.gd
@@ -13,7 +13,7 @@ func _physics_process(_delta: float) -> void:
 	
 func _on_body_entered(body: Node) -> void:
 	var impact_velocity = (last_velocity - linear_velocity).length()
-	print(impact_velocity)
+	
 	if !body.is_in_group("Player"):
 		if impact_velocity >  10.0:	
 			if !$BoxHitLoud.playing:
